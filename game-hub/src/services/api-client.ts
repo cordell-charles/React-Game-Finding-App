51a70fc1e0585cc268555d545ea6ee7aa7a30072
cordell-charles <cordellcharles95@gmail.com>
2025-01-06 14:47:50 +0000

    Creation of custon hook to get the games

--- game-hub/src/services/api-client.ts
+++ game-hub/src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: 'b9679ba2008c4c10a139abbea63fb551'
+        key: 'b9679ba2008c4c10a139abbea63fb551' // TEMP application: Todo - Move key to backend for safety
     }
 })
