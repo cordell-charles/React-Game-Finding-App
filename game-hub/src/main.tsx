963c216fb117a0657c3a1d7582e3a0c9ce97f2e7
cordell-charles <cordellcharles95@gmail.com>
2025-01-06 12:38:43 +0000

    Implementation of dark mode

--- game-hub/src/main.tsx
+++ game-hub/src/main.tsx
@@ -3,15 +3,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // Third party imports
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 
 // App imports
 import App from "./App";
+import theme from "./theme";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </React.StrictMode>
