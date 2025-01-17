1c21fbce939e8d8dcedb6582bd4b77d923e4fa8a
cordell-charles <cordellcharles95@gmail.com>
2025-01-17 15:14:16 +0000

    Adding missing import

--- game-hub/src/components/GameCardContainer.tsx
+++ game-hub/src/components/GameCardContainer.tsx
@@ -1,15 +1,16 @@
-import { Box } from '@chakra-ui/react'
+import { Box } from "@chakra-ui/react";
+import { ReactNode } from "react";
 
 interface Props {
-    children: ReactNode
+  children: ReactNode;
 }
 
 const GameCardContainer = ({ children }: Props) => {
   return (
     <Box width="300px" borderRadius={10} overflow="hidden">
-        {children}
+      {children}
     </Box>
-  )
-}
+  );
+};
 
-export default GameCardContainer
+export default GameCardContainer;
